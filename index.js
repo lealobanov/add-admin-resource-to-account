f853bb5a3077f3fc14ed4ebdb030918cb2562167
Lea Lobanov <lea.lobanov@icloud.com>
2024-12-21 03:07:18 +0400

    Simplify recipe fields

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 // Pass the repo name
 const recipe = "add-admin-resource-to-account";
 
-//Generate paths of each code file to render
-const contractPath = `${recipe}/cadence/contract.cdc`;
-const transactionPath = `${recipe}/cadence/transaction.cdc`;
-
-//Generate paths of each explanation file to render
-const smartContractExplanationPath = `${recipe}/explanations/contract.txt`;
-const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
-
 export const addAdminResourceToAccount = {
   slug: recipe,
   title: "Add Admin Resource to Account",
@@ -17,10 +9,6 @@ export const addAdminResourceToAccount = {
   playgroundLink:
     "https://play.onflow.org/a7d190b6-e0f1-4acc-b34c-f37b39fbab33?type=tx&id=1e1128cf-b88e-4f10-9877-247b71a62ee4&storage=none",
   excerpt: "When you want to give someone else access to the admin resource.",
-  smartContractCode: contractPath,
-  smartContractExplanation: smartContractExplanationPath,
-  transactionCode: transactionPath,
-  transactionExplanation: transactionExplanationPath,
   filters: {
     difficulty: "intermediate",
   },
